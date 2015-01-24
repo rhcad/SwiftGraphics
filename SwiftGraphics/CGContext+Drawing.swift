a18c8bfe22a1a3d8c185d980b6351b242987885d
Jonathan Wight <jwight@mac.com>
2015-01-24 10:45:20 -0800

    Recovered lost drawing code.

--- SwiftGraphics/CGContext+Drawing.swift
+++ SwiftGraphics/CGContext+Drawing.swift
@@ -7,3 +7,83 @@
 //
 
 import Foundation
+
+
+// TODO: This code is mostly experimental, use at your own risk - see TODO.markdown
+
+public extension CGContext {
+
+    func strokePath(path:CGPath) {
+        CGContextAddPath(self, path)
+        CGContextStrokePath(self)
+    }
+
+    func fillRect(rect:CGRect) {
+        CGContextFillRect(self, rect)
+    }
+
+    func strokeRect(rect:CGRect) {
+        CGContextStrokeRect(self, rect)
+    }
+
+    func strokeEllipseInRect(rect:CGRect) {
+        CGContextStrokeEllipseInRect(self, rect)
+    }
+
+    func strokeLines(points:[CGPoint]) {
+        points.withUnsafeBufferPointer {
+            (p:UnsafeBufferPointer<CGPoint>) -> Void in
+            CGContextStrokeLineSegments(self, p.baseAddress, UInt(points.count))
+        }
+    }
+
+    func strokeLine(p1:CGPoint, _ p2:CGPoint) {
+        strokeLines([p1, p2])
+    }
+
+    // TODO: Rename strokePolygon?
+    func strokeLine(points:[CGPoint], close:Bool = false) {
+        var newPoints:[CGPoint] = []
+        for (first, second) in GeneratorOf(SlidingWindow(points)) {
+            if second == nil {
+                if (close == true) {
+                    newPoints.append(first)
+                    newPoints.append(points[0])
+                }
+                break
+            }
+            newPoints.append(first)
+            newPoints.append(second!)
+        }
+
+        strokeLines(newPoints)
+    }
+
+    
+    func fillCircle(#center:CGPoint, radius:CGFloat) {
+        let rect = CGRect(center:center, size:CGSize(width:radius * 2, height:radius * 2))
+        CGContextFillEllipseInRect(self, rect)
+    }
+    
+    func fillCircle(circle:Circle) {
+        CGContextFillEllipseInRect(self, circle.frame)
+    }
+
+// MARK: Convenience shapes
+
+    func strokeCross(rect:CGRect) {
+        let linePoints = [
+            CGPoint(x:rect.minX, y:rect.midY), CGPoint(x:rect.maxX, y:rect.midY),
+            CGPoint(x:rect.midX, y:rect.minY), CGPoint(x:rect.midX, y:rect.maxY),
+        ]
+        strokeLines(linePoints)
+    }
+
+    func strokeSaltire(rect:CGRect) {    
+        let linePoints = [
+            CGPoint(x:rect.minX, y:rect.minY), CGPoint(x:rect.maxX, y:rect.maxY),
+            CGPoint(x:rect.minX, y:rect.maxY), CGPoint(x:rect.maxX, y:rect.minY),
+        ]
+        strokeLines(linePoints)
+    }
+}
