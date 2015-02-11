7a485f078e0f5a29c137d94e90080459015c59ff
Zhang Yungui <rhcad@hotmail.com>
2015-02-11 17:40:09 +0800

    Add CGPathFromSVGPath() to make path easily.

---
+++ SwiftGraphics/CGPathFromSVGPath.swift
@@ -0,0 +1,27 @@
+//
+//  CGPathFromSVGPath.swift
+//  SwiftGraphics
+//
+//  Created by Zhang Yungui on 2/11/15.
+//  Copyright (c) 2015 schwa.io. All rights reserved.
+//
+
+import CoreGraphics
+
+/**
+ Convenience function to create path from string like SVG path element.
+
+ :param: d path string as the ‘d’ attribute of SVG path. It begins with a ‘M’ character and can contain
+           instructions ‘MmLlCcQqSsTtZz’ as described in http://www.w3.org/TR/SVGTiny12/paths.html
+
+ :test:   CGPathFromSVG("M20,50 Q40,5 100,50").endPoint
+ :result: CGPoint((100,50))
+
+ :return: the new path.
+ */
+public func CGPathFromSVGPath(d:String) -> CGMutablePath
+{
+    let path = CGPathCreateMutable()
+    CGPathFromSVGPath(path, d)
+    return path
+}
