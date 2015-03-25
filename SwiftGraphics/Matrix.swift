74188a179968eb55ce7e3f7690dbe53f0a4a590e
Zhang Yungui <rhcad@hotmail.com>
2015-03-25 09:31:07 +0800

    Avoid the compiler error about fatalError for i386 Simulator.
    Validating podspec
    -> SwiftGraphics (0.0.4)
    - WARN | [SwiftGraphics/Core,SwiftGraphics/iOS] SwiftGraphics/SwiftGraphics/Matrix.swift:81:12: warning: will never be executed
    - NOTE | [SwiftGraphics/Core,SwiftGraphics/iOS] SwiftGraphics/SwiftGraphics/Matrix.swift:79:5: note: a call to a noreturn function

--- SwiftGraphics/Matrix.swift
+++ SwiftGraphics/Matrix.swift
@@ -76,7 +76,8 @@ public func * (lhs:Matrix, rhs:Matrix) -> Matrix {
     // See: http://stackoverflow.com/questions/26519169/matrix-multiplication-in-swift-using-accelerate-framework-32-bit-vs-64-bit
     // See cblas_sgemm
 
-    preconditionFailure("vDSP_mmul not defined for i386 Simulator")
+    //Comment the following line to avoid the compiler error about fatalError for i386 Simulator.
+    //preconditionFailure("vDSP_mmul not defined for i386 Simulator")
 #endif
     return Matrix(data:resultData, columns:resultColumns, rows:resultRows, stride:1)
 }
